399eba644544b9499cd81354c8743590c419f4bf
Hajo <hajo-groen@mail.com>
2023-01-11 20:33:57 +0100

    Started on rail network object with empty functions

---
+++ railNL/railNetwork.py
@@ -0,0 +1,48 @@
+from typing import List
+
+class railNetwork:
+    """
+    TODO
+    Network of stations connected by rail connections
+    """
+    def __init__(self, filenameStations: str, filenameConnections: str):
+        """
+        TODO
+        Initializer funtion
+        """
+        pass
+    
+    def loadStations(self, filename: str) -> None:
+        """
+        TODO
+        Loads station objects from file filename
+        """
+        pass
+    
+    def loadConnections(self, filename: str) -> None:
+        """
+        TODO
+        Adds rail connections to station objects
+        """
+        pass
+    
+    def listStations(self):
+        """
+        TODO
+        Returns a list of all stations
+        """
+        pass
+
+    def addRoute(self):
+        """
+        TODO
+        Creates an empty route object and adds it to routes
+        """
+        pass
+    
+    def listRoutes(self):
+        """
+        TODO
+        Returns the list of train routes
+        """
+        pass
