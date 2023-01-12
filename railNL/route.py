8f8371d10be5ec2ab3059612e918e9efacda4175
Hajo <hajo-groen@mail.com>
2023-01-12 14:23:42 +0100

    Implemented addconnection which checks whether an added rail connection does connect to part of the route implemented length, which returns the length of the route implemented liststations, which returns a list of all station names implemented brokenconnections, which checks for and returns any connection in the route that does not exist implemented duration which counts up all minutes in the route

--- railNL/route.py
+++ railNL/route.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple
+from typing import List, Tuple, Any
+from station import Station
 
 class Route:
     """
@@ -8,26 +9,57 @@ class Route:
 
     def __init__(self, id: int):
         """
-        TODO
         Initializer function
         """
         self.id = id
-    
-    def addConnection(self, station):
+        self.stations: List[Station] = []
+
+    def addConnection(self, station: Station, position: int = -1) -> bool:
         """
-        TODO
-        Adds a rail connection
+        Adds a rail connection at the specified position (0 for start, -1 for end)
         """
-    
+        if position in [0, -1] and not \
+            self.stations[position].hasConnection(self.stations[position].name()):
+            return False
+        
+        if position > 0 and not self.stations[position].hasConnection(self.stations[position-1].name()):
+            return False
+        
+        if position < 0 and not self.stations[position].hasConnection(self.stations[position+1].name()):
+            return False
+
+        self.stations.insert(position, station)
+
+
+    def length(self) -> int:
+        """
+        Returns amount of connections in the route
+        """
+        return len(self.stations)
+
     def listStations(self) -> List[str]:
         """
-        TODO
         returns a list of station names
         """
-    
-    def getTime(self) -> int:
-        """
-        TODO
-        Returns the current time of the 
-        """
+        return [station.name() for station in self.stations]
 
+    def brokenConnections(self) -> List[Tuple[str,str]]:
+        """Returns a list of all nonexistent rail connections"""
+        missing = []
+        for index in range(len(self.stations - 1)):
+            if not self.stations[index].hasConnection(self.stations[index + 1].name()):
+                missing.append[(self.stations[index].name(), self.stations[index + 1].name())]
+        
+        return missing
+
+    def duration(self) -> int:
+        """
+        Returns the total duration of the route
+        """
+        totalDuration = 0
+
+        for index in range(len(self.stations - 1)):
+            if self.stations[index].hasConnection(self.stations[index].name()):
+                totalDuration += self.stations[index].connectionDuration(self.stations[index].name())
+        
+        return totalDuration
