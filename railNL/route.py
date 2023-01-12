71478b983b913975d992217c89074fad53acf3b8
Hajo <hajo-groen@mail.com>
2023-01-12 08:58:09 +0100

    Created route object with basic functions

---
+++ railNL/route.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple
+
+class Route:
+    """
+    TODO
+    Route object that tracks rail connections between stations
+    """
+
+    def __init__(self, id: int):
+        """
+        TODO
+        Initializer function
+        """
+        self.id = id
+    
+    def addConnection(self, station):
+        """
+        TODO
+        Adds a rail connection
+        """
+    
+    def listStations(self) -> List[str]:
+        """
+        TODO
+        returns a list of station names
+        """
+    
+    def getTime(self) -> int:
+        """
+        TODO
+        Returns the current time of the 
+        """
+
