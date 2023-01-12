c3eeef666895e1b230a84e1de22f0116c3ac481c
Hajo <hajo-groen@mail.com>
2023-01-12 10:24:25 +0100

    added station functionality

--- railNL/station.py
+++ railNL/station.py
@@ -4,41 +4,55 @@ class Station:
     """
     Station object for a rail network
 
-
-    
+    Attributes:
+        name (str): The name of the station
+        position (Tuple[float, float]): The x and y coordinates of the station.
+        Connections (Dict[str, Tuple[Station, int]]): Connected stations and the time the rail
+            connection takes keyed by station name.
     """
 
-    def __init__(self, name: str, x: int, y: int):
-        """
-        TODO
-        Initializer function
-        """
+    def __init__(self, name: str, x: float, y: float):
+        """Initializer function"""
         self.name = name
         self.position = (x, y)
         self.connections: Dict[str, Tuple["Station", int]] = dict()
-        pass
+
+    def getName(self):
+        """Returns the name of the station"""
+        return self.name
 
     def addConnection(self, otherStation: "Station", duration: int):
         """
-        TODO
         Adds a connection to station with a duration
         """
+        self.connections[otherStation.getName()] = (otherStation, duration)
     
+    def removeConnection(self, stationName: str):
+        """
+        Removes a station from connections
+        """
+        self.connections.pop(stationName)
 
-    def listConnections(self) -> List(str):
+    def listConnections(self) -> List[Tuple(str, int)]:
         """
-        TODO
-        Returns a list names of all connected stations.
+        Returns a list of tuples of station name and duration for a connection.
         """
+        return [(key, duration) for key, (_, duration)  in self.connections.items()]
+    
+    def getConnectedStation(self, stationName: str):
+        """
+        returns the station object of a connected station.
+        """
+        return self.connections[stationName][0]
 
-    def connectionTime(self, stationName: str) -> int:
+    def connectionDuration(self, stationName: str) -> int:
         """
-        TODO
         Returns the duration of the connection
         """
+        return self.connections[stationName][1]
 
     def hasConnection(self, stationName: str) -> bool:
         """
-        TODO
         Returns True if the station is connected to the station with stationName, else False.
         """
+        return stationName in self.listConnections()
