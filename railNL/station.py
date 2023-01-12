de0da29399407756acd9f788e2b674b36dcf7150
Hajo <hajo-groen@mail.com>
2023-01-12 09:19:33 +0100

    renamed variable

--- railNL/station.py
+++ railNL/station.py
@@ -1,30 +1,44 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 class Station:
+    """
+    Station object for a rail network
+
+
+    
+    """
+
     def __init__(self, name: str, x: int, y: int):
         """
         TODO
         Initializer function
         """
         self.name = name
-        self.coordinates = (x, y)
-        self.connections = []
+        self.position = (x, y)
+        self.connections: Dict[str, Tuple["Station", int]] = dict()
         pass
 
-    def addConnection(self, station: "Station", duration: int):
+    def addConnection(self, otherStation: "Station", duration: int):
         """
         TODO
         Adds a connection to station with a duration
         """
+    
 
-    def listConnections(self):
+    def listConnections(self) -> List(str):
         """
         TODO
         Returns a list of all connections the station has to other stations.
         """
 
-    def connectionTime(self, stationName) -> int:
+    def connectionTime(self, stationName: str) -> int:
         """
         TODO
         Returns the duration of the connection
+        """
+
+    def hasConnection(self, stationName: str) -> bool:
+        """
+        TODO
+        Returns True if 
         """
