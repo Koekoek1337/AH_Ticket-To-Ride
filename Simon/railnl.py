5fb0f5c6c1bd938a25f04d78b1a2d725e01b4454
simon2704 <sdejong2704@gmail.com>
2023-01-13 11:48:14 +0100

    tweede upload

--- Simon/railnl.py
+++ Simon/railnl.py
@@ -5,6 +5,20 @@ class Network():
     def __init__(self):
         self.stations = {}
 
+    def load_station(self, filename: str):
+        """ Load all stations. """
+        with open(filename,'r') as file:
+            header = file.readline()
+            for line in file:
+                splits = line.split(',')
+                if len(splits)>2:
+                    station_name = str(splits[0])
+                    y = float(splits[1])
+                    x = float(splits[2])
+                    if station_name not in self.stations:
+                        self.stations[station_name] = Station(station_name)
+
+
     def load_connections(self, filename: str):
         """ Loads and add all the possible connections with duration to each station. """
         with open(filename, 'r') as file:
@@ -16,25 +30,13 @@ class Network():
                     station_2 = str(splits[1])
                     distance = int(splits[2])
                     if station_1 in self.stations:
-                        self.station[(station_2, distance)]
+                        self.stations[station_1].add_connection(station_2, distance)
                     elif station_2 in self.stations:
-                        self.station[(station_1, distance)]
-
-
-    def load_station(self, filename: str):
-        """ Load all stations. """
-        with open(filename,'r') as file:
-            header = file.readline()
-            for line in file:
-                splits = line.split(',')
-                if len(splits)>2:
-                    station_name = str(splits[0])
-                    if station_name not in self.stations:
-                        self.stations[station_name] = station
+                        self.stations[station_2].add_connection(station_1, distance)
 
 class Station():
 
-    def __init__(self, station_name, connections):
+    def __init__(self, station_name):
         self.station_name: str = station_name
         self.connections = []
 
@@ -45,7 +47,8 @@ class Station():
 
 
 
+
 if __name__ == "__main__":
-    station = Station()
-    station.load_station("StationsHolland.csv")
-    # station.load_connections("ConnectiesHolland.csv")
+    network = Network()
+    network.load_station("StationsHolland.csv")
+    network.load_connections("ConnectiesHolland.csv")
