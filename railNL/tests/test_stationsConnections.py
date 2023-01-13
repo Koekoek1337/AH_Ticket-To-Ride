5c1b6379a9cf2687f835f789488076653ffa6ebe
Hajo <hajo-groen@mail.com>
2023-01-13 13:46:51 +0100

    Added test for station and connection functionality

---
+++ railNL/tests/test_stationsConnections.py
@@ -0,0 +1,62 @@
+from classes.station import Station
+from classes.connection import Connection
+
+from typing import Tuple
+
+import pytest
+
+@pytest.mark.parametrize("name, x, y", [("AAA", 0, 0), ("BBB", 0.5, 0.7)])
+def test_EmptyStation(name, x, y):
+    station = Station(name, x, y)
+
+    assert station.name() == name
+    assert station.position() == (x, y)
+    assert station.connectionAmount() == 0
+    assert station.listConnections() == []
+    assert station.hasConnection(name) == False
+
+def generateConnectionNew(stationA, stationB, time) -> Connection:
+    return Connection(0, stationA, stationB, time)
+
+
+# initialize small rail system
+stationA = Station("AAA", 0, 0)
+stationB = Station("BBB", 0, 0)
+stationC = Station("CCC", 0, 0)
+stationD = Station("DDD", 0, 0)
+
+connection0 = Connection(0, stationA, stationB, 10)
+stationA.addConnection("BBB", connection0)
+stationB.addConnection("AAA", connection0)
+
+connection1 = Connection(1, stationB, stationC, 10)
+stationB.addConnection("CCC", connection1)
+stationC.addConnection("BBB", connection1)
+
+connection2 = Connection(2, stationB, stationD, 10)
+stationB.addConnection("DDD", connection2)
+stationD.addConnection("BBB", connection2)
+
+connection3 = Connection(3, stationC, stationD, 10)
+stationC.addConnection("DDD", connection3)
+stationD.addConnection("CCC", connection3)
+
+
+def test_connectionAmount():
+    assert stationA.connectionAmount() == 1
+    assert stationB.connectionAmount() == 3
+    assert stationC.connectionAmount() == 2
+    assert stationD.connectionAmount() == 2
+
+
+def test_listConnections():
+    assert stationB.listConnections() == [("AAA", 10, 1), ("CCC", 10, 2), ("DDD", 10, 2)]
+
+def test_hasConnections():
+    assert stationA.hasConnection("BBB") == True
+
+def test_getConnectedStation():
+    assert stationA.getConnectedStation("BBB") == stationB
+
+def test_connectionDuration():
+    assert stationA.connectionDuration("BBB") == 10
