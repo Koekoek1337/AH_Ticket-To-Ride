186feb27562f177bb856e467bbc7c6012048fcab
Hajo <hajo-groen@mail.com>
2023-01-13 13:12:34 +0100

    Changed type hints added ID getter fixed typo in getconnectedstation Added connectionpoints function that returns coordinate pairs for visualization

--- railNL/classes/connection.py
+++ railNL/classes/connection.py
@@ -1,8 +1,7 @@
-import station
+from typing import TYPE_CHECKING
 
-from typing import TypeAlias
-
-Station: TypeAlias = station.Station
+if TYPE_CHECKING:
+    from station import Station
 
 class Connection:
     """
@@ -11,20 +10,27 @@ class Connection:
     - Implement in station
     - Implement in route
     """
-    def __init__(self, uid: int, stationA: Station, stationB: Station, duration: int):
+
+    def __init__(self, uid: int, stationA: "Station", stationB: "Station", duration: float):
         """Initializer function"""
         self._id = uid
         self._connectedStations = {stationA.name():stationA, stationB.name():stationB}
         self._duration = duration
 
+    def getID(self):
+        return self._id
+
     def duration(self):
         """Returns the duration of the connection"""
         return self._duration
-    
-    def getConnectedStation(self, name):
+
+    def getConnectedStation(self, name: str) -> "Station":
         """Returns the station object keyed with name"""
-        return self._connectedStations(name)
+        return self._connectedStations[name]
 
     def stationConnectionAmount(self, name:str):
         """Returns the amount of connections of station keyed with name"""
         return self._connectedStations[name].connectionAmount()
+    
+    def connectionPoints(self):
+        return list(self._connectedStations.items())[0][1].position(), list(self._connectedStations.items())[1][1].position()
