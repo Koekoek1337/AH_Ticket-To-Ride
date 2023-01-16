cd72b0a980b96cf90107bcd82a8d59e3e51f71f7
Hajo <hajo-groen@mail.com>
2023-01-16 16:28:01 +0100

    Added testcode for visualization and result exporting

--- railNL/__main__.py
+++ railNL/__main__.py
@@ -3,8 +3,14 @@ from visualize.visualize import visualize
 
 def main():
     network = RailNetwork("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
+    
+    network.createRoute("Den Helder")
+    network.routeAppendStation(0, "Alkmaar")
+    network.routeAppendStation(0, "Castricum")
 
-    visualize(network.connectionPoints(), network.stationPoints())
+    visualize(network.connectionPoints(), network.stationPoints(), network.routePointLists())
+
+    network.exportSolution("Nederland")
 
 if __name__ == "__main__":
     main()
