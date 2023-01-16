c711521de915a3219c3fc162db5d12d62ffd0ec3
Hajo <hajo-groen@mail.com>
2023-01-16 16:14:56 +0100

    Implemented list of connections in route Edited duration to use connection implementation Implemented insertStation with connection implementation implemented appendStation as macro for insertStation at endpoint Implemented insertconnection for use by insertStation implemented replaceconnecton for use by insertStation Imlemented findconnection that finds the connection node between two stations implemented removeStation that removes a station and its corresponding connections implemented removeConnections that removes the connections around an index implemented removeConnection that removes a connection from an index edited the method that seeks broken links added connectionpoints method that returns all points for the visualization added empty method, that removes the route from all connections and stations

--- railNL/classes/route.py
+++ railNL/classes/route.py
@@ -1,4 +1,5 @@
 from classes.station import Station
+from classes.connection import Connection
 
 from typing import List, Tuple, Any
 
@@ -12,62 +13,184 @@ class Route:
 
     @classmethod
     def newID(self):
+        """Returns an unique integer for route ID"""
         self.counter += 1
         return self.counter
 
-    def __init__(self):
-        """
-        Initializer function
-        """
-        self.id = self.newID()
-        self.stations: List[Station] = []
+    def __init__(self, rootStation: Station):
+        """Initializer function"""
 
-    def addConnection(self, station: Station, position: int = -1) -> bool:
-        """
-        Adds a rail connection at the specified position (0 for start, -1 for end)
-        """
-        if position in [0, -1] and not \
-            self.stations[position].hasConnection(self.stations[position].name()):
-            return False
-        
-        if position > 0 and not self.stations[position].hasConnection(self.stations[position-1].name()):
-            return False
-        
-        if position < 0 and not self.stations[position].hasConnection(self.stations[position+1].name()):
-            return False
+        self._id = self.newID()
+        self._stations: List[Station] = [rootStation]
+        self._connections: List[Connection] = []
 
-        self.stations.insert(position, station)
+        rootStation.addRoute(self.getID())
+    
+    def getID(self):
+        """Returns the id of the route"""
+        return self._id
 
-
-    def length(self) -> int:
-        """
-        Returns amount of connections in the route
-        """
-        return len(self.stations)
-
-    def listStations(self) -> List[str]:
-        """
-        returns a list of station names
-        """
-        return [station.name() for station in self.stations]
-
-    def brokenConnections(self) -> List[Tuple[str,str]]:
-        """Returns a list of all nonexistent rail connections"""
-        missing = []
-        for index in range(len(self.stations - 1)):
-            if not self.stations[index].hasConnection(self.stations[index + 1].name()):
-                missing.append[(self.stations[index].name(), self.stations[index + 1].name())]
-        
-        return missing
-
-    def duration(self) -> int:
+    def duration(self) -> float:
         """
         Returns the total duration of the route
         """
         totalDuration = 0
 
-        for index in range(len(self.stations - 1)):
-            if self.stations[index].hasConnection(self.stations[index].name()):
-                totalDuration += self.stations[index].connectionDuration(self.stations[index].name())
+        for connection in self._connections:
+            if connection:
+                totalDuration += connection.duration()
         
         return totalDuration
+
+    def length(self) -> int:
+        """
+        Returns amount of connections in the route
+        """
+        return len(self._connections)
+    
+    def listStations(self) -> List[str]:
+        """
+        returns a list of station names
+        """
+        return [station.name() for station in self._stations]
+
+    def insertStation(self, stationIndex: int, station: Station) -> None:
+        """
+        Inserts a rail connection at the specified Index (0 for start, -1 end)
+
+        Args:
+            station (Station): The station node to connect to
+        """
+        self._stations.insert(stationIndex, station)
+        station.addRoute(self.getID())
+
+        if stationIndex < 0:
+            stationIndex += self.length()
+
+        if stationIndex == 0:
+            self.insertConnection(stationIndex, stationIndex)
+            return
+
+        if stationIndex > 0:
+            self.insertConnection(stationIndex, stationIndex - 1)
+
+        if stationIndex < len(self._stations) - 1:
+            self.replaceConnection(stationIndex,  stationIndex)
+    
+    def appendStation(self, station: Station) -> None:
+        """
+        Calls insertStation for the end of self._stations
+        """
+        self.insertStation(len(self._stations), station)
+    
+    def insertConnection(self, stationIndex: int, connectionIndex: int):
+        """Insert connection to station on connectionIndex"""
+        print(self._stations)
+        if stationIndex == 0:
+            connection = self.findConnection(self._stations[stationIndex], self._stations[stationIndex + 1])
+
+        else:
+            connection = self.findConnection(self._stations[stationIndex], self._stations[stationIndex - 1])
+
+
+        self._connections.insert(connectionIndex, connection)
+
+    def replaceConnection(self, stationIndex: int, connectionIndex: int):
+        """replace connection object on connectiop Index with new connection"""
+        connection = self.findConnection(self._stations[stationIndex], 
+                                         self._stations[stationIndex + 1]
+                                        )
+        self._connections[connectionIndex] = connection
+
+    def findConnection(self, station1: Station, station2: Station):
+        """
+        Returns the connection between station1 and station2 if it exists
+        """
+
+        if not station1.hasConnection(station2.name()):
+            return None
+
+        connection = station1.getConnection(station2.name())
+        connection.addRoute(self.getID())
+        
+        return connection
+    
+    def removeStation(self, station: Station) -> None:
+        """
+        Removes a station from stations
+        TODO
+        find replacement connection
+        """
+        for stationIndex in range(len(self._connections)):
+            if self._stations[stationIndex].name() == station.name():
+                self._stations[stationIndex].removeRoute(self.getID())
+                self.removeConnections(stationIndex)
+        
+    def removeConnections(self, stationIndex: int) -> None:
+        """Removes connections around station on Index"""
+        if stationIndex < len(self._stations) - 1:
+            self.removeConnection(stationIndex)
+
+        if stationIndex > 0:
+            self.removeConnection(stationIndex - 1)
+
+    def removeConnection(self, connectionIndex: int):
+        """
+        Removes a connection from Index.
+        """
+
+        if self._connections[connectionIndex]:
+            self._connections[connectionIndex].removeRoute(self.getID())
+
+        self._connections.pop(connectionIndex)
+
+    def insertAfter(self, targetStation: Station, newStation: Station):
+        """
+        TODO
+
+        Insert the station after the station with name stationName
+        """
+        pass
+
+    def insertBefore(self, targetStation: Station, newStation: Station):
+        """
+        TODO
+
+        Insert the station before the station with name stationName
+        """
+        pass
+
+    def brokenConnections(self) -> List[Tuple[str,str]]:
+        """Returns a list of all nonexistent rail connections"""
+        missing = []
+
+        for i in range(self.length()):
+            if not self._connections[i]:
+                missing.append((self._stations[i].name(), self._stations[i + 1].name()))
+        
+        return missing
+
+    def connectionPoints(self) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
+        pointPairs = []
+        
+        for connection in self._connections:
+            if connection:
+                pointPairs.append(connection.connectionPoints())
+        
+        return pointPairs
+
+    def empty(self) -> None:
+        """
+        Remove all stations and connections and remove route from them.
+        """
+
+        for station in self._stations:
+            station.removeRoute(self.getID())
+        
+        self._stations = []
+
+        for connection in self._connections:
+            if connection:
+                connection.removeRoute(self.getID())
+        
+        self._connections = []
