354b006eed085c7d323b1fafcf0a57f90662e6ab
Hajo <hajo-groen@mail.com>
2023-01-17 11:22:09 +0100

    reworked the listStations functions

--- railNL/classes/station.py
+++ railNL/classes/station.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, List, Tuple, Dict
+from typing import TYPE_CHECKING, List, Tuple, Dict, Optional
 
 if TYPE_CHECKING:
     from connection import Connection
@@ -29,39 +29,147 @@ class Station:
         """"""
         return f"Station({self._name}, {self._position[0]}. {self._position[1]})"
 
-    def __str__(self) -> str:
-        """string representation"""
-
-        return f"{self._name} station, connected to {len(self.listConnections())} other stations"
-
     def name(self) -> str:
         """Returns the name of the station"""
 
         return self._name
     
     def position(self) -> Tuple[float, float]:
-        """Returns the position of the station"""
+        """Returns the x and y coordinates of the station"""
     
         return self._position
 
-    def stationPoint(self) -> Tuple[str, Tuple[float, float]]:
-        """Returns the name and position of the station"""
+    def listStations(self, nConnections = False, nUnused = False, nUnconnected = False) \
+        -> List[List["Station", Optional[int], Optional[int], Optional[int]]]:
+        """
+        Returns a list of all connected station nodes with the duration of the connection and
+        optional information on the connections
+        
+        Args:
+            nConnections (bool): Whether the amount of connections of the stations should be given.
+            nUnused (bool): Whether the amount of unused connections should be given. A connection
+                            is unused if the corresponding station node is in a route, but the
+                            connection is not.
+            nUnconnected (bool): Whether the amount of unconnected connections should be given. A
+                               connection is unconnected if the corresponding station node is not
+                               in any route.
+        
+        Returns: A list of lists of The station nodes with the duration of the connection amounts of
+            connections (optional), the amount of unused connections (optional) and the amount of 
+            unconnected connections (optional) in that order
+        """
+        stationList = []
 
-        return self.name(), self.position()
+        for stationName in self._connections.keys():
+            station = self.getConnectedStation(stationName)
+            
+            stationPoint = [station, self.connectionDuration(stationName)]
 
-    def addConnection(self, stationName,  connection: "Connection") -> None:
-        """Adds a connection to station with a duration"""
+            if nConnections:
+                stationPoint.append(station.connectionAmount())
+            
+            if nUnused:
+                stationPoint.append(station.unusedConnectionAmount())
+            
+            if nUnconnected:
+                stationPoint.append(station.unvistitedConnectionAmount())
+        
+            stationList.append(stationPoint)
+        
+        return stationList
 
-        self._connections[stationName] = connection
+    def listConnectedStations(self, nConnections = False, nUnused = False, nUnconnected = False) \
+        -> List[List["Station", Optional[int], Optional[int], Optional[int]]]:
+        """
+        Returns a list of connected station nodes that are not in any route with the duration of the
+        connection and optional information on the connections
+        
+        Args:
+            nConnections (bool): Whether the amount of connections of the stations should be given.
+            nUnused (bool): Whether the amount of unused connections should be given. A connection
+                            is unused if the corresponding station node is in a route, but the
+                            connection is not.
+            nUnconnected (bool): Whether the amount of unconnected connections should be given. A
+                               connection is unconnected if the corresponding station node is not
+                               in any route.
+        
+        Returns: A list of lists of The station nodes with the duration of the connection amounts of
+            connections (optional), the amount of unused connections (optional) and the amount of 
+            unconnected connections (optional) in that order
+        """
+        stationList = []
+        
+        for stationName in self._connections.keys():
+            station = self.getConnectedStation(stationName)
+            
+            if station.isConnected():
+                continue
 
-    def addRoute(self, routeID: int):
-        """Adds a route to the set of routes"""
-        self._routes.add(routeID)
-    
-    def removeRoute(self, routeID: int):
-        """Removes a route from the set of routes"""
-        if routeID in self._routes:
-            self._routes.remove(routeID)
+            stationPoint = [station, self.connectionDuration(stationName)]
+
+            if nConnections:
+                stationPoint.append(station.connectionAmount())
+            
+            if nUnused:
+                stationPoint.append(station.unusedConnectionAmount())
+            
+            if nUnconnected:
+                stationPoint.append(station.unvistitedConnectionAmount())
+        
+            stationList.append(stationPoint)
+        
+        return stationList
+        
+    def listUnusedConnections(self, nConnections = False, nUnused = False, nUnconnected = False) \
+        -> List[List["Station", Optional[int], Optional[int], Optional[int]]]:
+        """
+        Returns a list of connected station nodes to which the connections are not in any route
+        with the duration of the connection and optional information on the connections
+        
+        Args:
+            nConnections (bool): Whether the amount of connections of the stations should be given.
+            nUnused (bool): Whether the amount of unused connections should be given. A connection
+                            is unused if the corresponding station node is in a route, but the
+                            connection is not.
+            nUnconnected (bool): Whether the amount of unconnected connections should be given. A
+                               connection is unconnected if the corresponding station node is not
+                               in any route.
+        
+        Returns: A list of lists of The station nodes with the duration of the connection amounts of
+            connections (optional), the amount of unused connections (optional) and the amount of 
+            unconnected connections (optional) in that order
+        """
+        unusedStations = []
+        
+        for name, connection in self._connections.items():
+            if connection.getConnectedStation(name).isConnected():
+                unusedStations.append(
+                    (name, connection.duration(), 
+                     connection.getConnectedStation(name).unvistitedConnectionAmount())
+                    )
+
+        stationList = []
+        
+        for stationName, connection in self._connections.items():
+            if connection.isConnected():
+                continue
+        
+            station = self.getConnectedStation(stationName)
+
+            stationPoint = [station, self.connectionDuration(stationName)]
+
+            if nConnections:
+                stationPoint.append(station.connectionAmount())
+            
+            if nUnused:
+                stationPoint.append(station.unusedConnectionAmount())
+            
+            if nUnconnected:
+                stationPoint.append(station.unvistitedConnectionAmount())
+        
+            stationList.append(stationPoint)
+        
+        return stationList
 
     def connectionAmount(self) -> int: 
         """Returns the amount of stations connected to the station"""
@@ -75,7 +183,7 @@ class Station:
         """
         unvisitedStations = 0
         for name, connection in self._connections.items():
-            if connection.getConnectedStation(name).isVisited():
+            if connection.getConnectedStation(name).isConnected():
                 unvisitedStations += 1
         
         return unvisitedStations
@@ -87,69 +195,51 @@ class Station:
         """
         unusedStations = 0
         for name, connection in self._connections.items():
-            if connection.isVisited():
+            if connection.isConnected():
                 unusedtations += 1
         
         return unusedStations
 
-    def listConnections(self) -> List[Tuple[str, float, int]]:
+    def getConnectedStation(self, stationName: str) -> "Station":
+        """returns the station object of a connected station."""
+        return self._connections[stationName].getConnectedStation(stationName)
+
+    def connectionDuration(self, stationName: str) -> int:
+        """Returns the duration of the connection"""
+        return self._connections[stationName].duration()
+
+    # under the hood
+    def hasConnection(self, stationName: str) -> bool:
         """
-        Returns (List[Tuple[str, float, int]]): name, duration and connections of all connecting 
-            stations
+        Returns True if the station is connected to the station with stationName, else False.
         """
-        return [(stationName, connection.duration(), connection.stationConnectionAmount(stationName)) 
-                for stationName, connection  in self._connections.items()]
+        return stationName in self._connections.keys()
+
+    def addConnection(self, stationName: str,  connection: "Connection") -> None:
+        """
+        Adds a connection node to the station.
+        
+        Args:
+            stationName (str): The name of the connected station.
+            connection (Connection): The connnection node
+        """
+        self._connections[stationName] = connection
+
+    def addRoute(self, routeID: int):
+        """Adds a route to the set of routes"""
+        self._routes.add(routeID)
     
-    def isVisited(self) -> bool:
+    def removeRoute(self, routeID: int):
+        """Removes a route from the set of routes"""
+        if routeID in self._routes:
+            self._routes.remove(routeID)
+
+    def isConnected(self) -> bool:
         """
         Returns true if the station is in any route, else false
         """
         return bool(self._routes)
 
-    def listUnvisitedConnections(self) -> List[Tuple[str, float, int]]:
-        """
-        Returns (List[Tuple[str, float, int]]): name, duration and unvisited connections of all 
-        connecting unvisited stations
-        """
-        unvisitedStations = []
-        
-        for name, connection in self._connections.items():
-            if connection.getConnectedStation(name).isVisited():
-                unvisitedStations.append(
-                    (name, connection.duration(), 
-                     connection.getConnectedStation(name).unvistitedConnectionAmount())
-                    )
-        
-        return unvisitedStations
-        
-    def listUnusedConnections(self) -> List[Tuple[str, float, int]]:
-        """
-        Returns (List[Tuple[str, float, int]]): name, duration and unvisited connections of all 
-        stations connected via an unused connection
-        """
-        unusedStations = []
-        
-        for name, connection in self._connections.items():
-            if connection.getConnectedStation(name).isVisited():
-                unusedStations.append(
-                    (name, connection.duration(), 
-                     connection.getConnectedStation(name).unvistitedConnectionAmount())
-                    )
-        
-        return unusedStations
-
-    def hasConnection(self, stationName: str) -> bool:
-        """Returns True if the station is connected to the station with stationName, else False."""
-        return stationName in self._connections.keys()
-
-    def getConnectedStation(self, stationName: str) -> "Station":
-        """returns the station object of a connected station."""
-        return self._connections[stationName].getConnectedStation(stationName)
-    
     def getConnection(self, stationName: str) -> "Connection":
-        """retunrs the connection object between a connected station"""
+        """returns the connection object between a connected station"""
         return self._connections[stationName]
-
-    def connectionDuration(self, stationName: str) -> int:
-        """Returns the duration of the connection"""
-        return self._connections[stationName].duration()
