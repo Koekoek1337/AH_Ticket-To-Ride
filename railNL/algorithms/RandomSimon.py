b4dd6688c86ee58d0e02e9fc0da6caf4cfe495b1
Thrasymachus-642 <finn@DESKTOP-K61CM0G.localdomain>
2023-01-18 13:56:40 +0100

    Simons harde werk

--- railNL/algorithms/RandomSimon.py
+++ railNL/algorithms/RandomSimon.py
@@ -2,27 +2,41 @@ from classes.railNetwork import RailNetwork
 from classes.route import Route
 from classes.station import Station
 
-def main():
-    model = RailNetwork(filename)
-    model.newRoute(random.Randomchoice(model.listStations()))
+import random
+
+tMax = 120
+routeMax = 7
+
+def Random():
+    iterations = 0
+    model = RailNetwork("StationsNationaal.csv", "ConnectiesNationaal.csv")
+    # Starts a route at a random station.
+    model.createRoute(random.choice(model.listStations()))
 
     model.nRoute() <= 7
 
-
-    while not checkValidSolution() and (model.validMoves() and model.nRoutes() <=7):
-        currentRoute = random.Randomchoice(model.listRoutes())
+    # Continue the routes until it satisfies the solution or exceed the timelimit.
+    while not model.checkValidSolution(tMax) and (model.hasLegalMoves(tMax, routeMax) and model.nRoute() <= routeMax):
+        iterations += 1
+        # The route connects to a random station to who it is connected.
+        currentRoute = random.choice(model.listRoutes())
         current_station = currentRoute.listStations()[-1]
-        new_station = random.Randomchoice(current_station.listConnectedStations())
-        currentRoute.appendStation(new_station)
+        new_station = random.choice(current_station.listStations())
+        currentRoute.appendStation(new_station[0])
 
-        if model.nRoute() < 7:
-            if model.validMoves() == True:
+        #  If there are not 7 trajects, there is a random chance, that the route conitinues or a new traject is made.
+        if model.nRoute() < routeMax:
+            if model.hasLegalMoves(tMax, routeMax) == True:
                 randomFloat = random.random()
                 if randomFloat <= 0.2:
-                    model.newRoute(random.Randomchoice(model.listStatins()))
+                    model.createRoute(random.choice(model.listStations()))
                 else:
                     continue
             else:
                 continue
-            
-    exportSolution("random")
+
+
+    print(f"total iterations: {iterations}")
+    print(model.listRoutes())
+
+    #exportSolution("random")
