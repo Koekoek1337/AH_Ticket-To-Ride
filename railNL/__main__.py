9f26e521ebdeac1fdac6e383e7ebf034be619176
Hajo <hajo-groen@mail.com>
2023-01-19 00:55:07 +0100

    Added new feature

--- railNL/__main__.py
+++ railNL/__main__.py
@@ -1,8 +1,20 @@
 from classes.railNetwork import RailNetwork
 from visualize.visualize import visualize
+from os import path
+
+BATCH = False
+
+if path.exists("railNL/algorithms/random_hajo.py"):
+    from algorithms import random_hajo
+
+    BATCH = True
 
 def main():
-    network = RailNetwork("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
+    network = RailNetwork("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
+    
+    if BATCH:
+        random_hajo.main(network, 7, 120)
+        return
     
     network.createRoute(network.getStation("Den Helder"))
     route = network.getRoute(0)
@@ -11,7 +23,7 @@ def main():
 
     visualize(network.connectionPoints(), network.stationPoints(), network.routePointLists())
 
-    network.exportSolution("Nederland")
+    network.exportSolution("results", "Nederland")
 
 if __name__ == "__main__":
     main()
