22e4b38c12db5da2444ecdecb78ed6f123d978ae
Hajo <hajo-groen@mail.com>
2023-01-23 10:37:42 +0100

    Edited variableName in insertStation for clarity Fixed bug in replaceConnection where the route would be removed from the station if it was popped, even when it was still in the route. Same goes for replaceConnection, which was also edited for clarity implemented replacementConnection for popStation when a station is popped in the middle of the route Fixed same bug as before in removeConnection, and edited for clarity Decided against listing brokenConnections in openStations.

--- railNL/classes/route.py
+++ railNL/classes/route.py
@@ -92,7 +92,9 @@ class Route:
         self._insertConnection(stationIndex, connectionIndex)
 
         if stationIndex < self.nStations() - 1:
-            self.replaceConnection(stationIndex, stationIndex)
+            connectionIndex = stationIndex
+
+            self.replaceConnection(stationIndex, connectionIndex)
 
     def _insertConnection(self, stationIndex: int, connectionIndex: int) -> int:
         """
@@ -118,14 +120,17 @@ class Route:
 
     def replaceConnection(self, stationIndex: int, connectionIndex: int):
         """replace connection object on connectioIndex with new connection"""
-        connection = self._findConnection(self._stations[stationIndex], 
+        oldConnection = self._connections[connectionIndex]
+        newConnection = self._findConnection(self._stations[stationIndex], 
                                          self._stations[stationIndex + 1]
                                         )
+       
+        self._connections[connectionIndex] = newConnection
 
-        if self._connections[connectionIndex]:
+        # Only attempt to remove routeID if oldConnection not None
+        # Do not remove routeID from connection if it is still in the route
+        if oldConnection and oldConnection not in self._connections:
             self._connections[connectionIndex].removeRoute(self.getID())
-
-        self._connections[connectionIndex] = connection
 
     def _findConnection(self, station1: Station, station2: Station) -> Union[Connection, None]:
         """
@@ -142,17 +147,20 @@ class Route:
     def popStation(self, stationIndex: int = -1) -> None:
         """
         Removes a station from stations at stationIndex
-        
-        TODO
-        - find replacement connection when not at edges
-        - prevent routeID from being removed if the station or connection is still in the route
         """
         
-        self._stations[stationIndex].removeRoute(self.getID())
+        station = self._stations.pop(stationIndex)
         
+        # Do not remove the routeID from station if it still occurs in the route
+        if station not in self._stations:
+            station.removeRoute(self.getID())
+
+
         self.removeConnections(stationIndex)
-        
-        self._stations.pop[stationIndex]
+
+        # Insert a new connection if possible if the removed station was not at the head or tail end
+        if stationIndex not in [0, self.nStations()]:
+            self._insertConnection(stationIndex, stationIndex - 1)
         
     def removeConnections(self, stationIndex: int) -> None:
         """Removes connections around station on Index"""
@@ -164,11 +172,13 @@ class Route:
 
     def removeConnection(self, connectionIndex: int):
         """
-        Removes a connection from Index.
+        Removes the connection on connectionIndex.
         """
-        self._connections[connectionIndex].removeRoute(self.getID())
-        
-        self._connections.pop(connectionIndex)
+        connection = self._connections.pop(connectionIndex)
+
+        # Do not remove routeID from connection if it is still in the route
+        if connection not in self._connections:
+            connection.removeRoute(self.getID())
 
     def getStation(self, index: int) -> Station:
         """Returns the station on stationIndex"""
@@ -178,21 +188,10 @@ class Route:
         """
         Returns (List[Tuple[Station, int]]): A list of tuples of stations and their indexes.
             a station can occur multiple times if it has a broken connection.
-        
-        TODO
-        - reimplement brokenConnections
 
         """
-        # brokenConnections = self.brokenConnections()
-
-        # The station at the head is always open
         openStations = [(self._stations[0], 0)]
 
-        # Stations with broken connections are open
-        # openStations += [stationA for stationA, stationB in brokenConnections]
-        # openStations += [stationB for stationA, stationB in brokenConnections]
-
-        # Stations at the tail are always open
         openStations += [(self._stations[self.nStations() - 1], self.nStations() - 1)]
 
         return openStations
