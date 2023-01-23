95b0c9a372f3afb51dfc7fd964ad4012330b2683
Hajo <hajo-groen@mail.com>
2023-01-23 09:30:33 +0100

    created file

--- railNL/algorithms/hillClimber_Hajo.py
+++ railNL/algorithms/hillClimber_Hajo.py
@@ -1,3 +1,14 @@
 """
+random solution with n routes
 
+solve(initialNetwork)
+    Gradient(initialSolution) -> 
+        best = initial
+        
+        while not converged:
+            randomSolution = solveRandom(deepcopy(initialSolution))
+
+            Tempered() -> find optimized solution for a random solution
+                attempt to add a random greedy route or remove a random route
+        
 """
