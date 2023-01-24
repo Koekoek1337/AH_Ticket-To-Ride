5197b73659e3ee569b4fdd66d08128df83a8cc0f
Hajo <hajo-groen@mail.com>
2023-01-24 12:20:49 +0100

    Started work on simple hillclimber algorithm based on random.

--- railNL/algorithms/hillClimber_Hajo.py
+++ railNL/algorithms/hillClimber_Hajo.py
@@ -1,14 +1,39 @@
-"""
-random solution with n routes
+import random
+import datetime
+import time
 
-solve(initialNetwork)
-    Gradient(initialSolution) -> 
-        best = initial
-        
-        while not converged:
-            randomSolution = solveRandom(deepcopy(initialSolution))
+from copy import deepcopy
 
-            Tempered() -> find optimized solution for a random solution
-                attempt to add a random greedy route or remove a random route
-        
+from classes.railNetwork import RailNetwork
+from classes.route import Route
+from classes.station import Station
+
+from typing import List, Dict, Union
+
+
+import algorithms.random_hajo as randomAlgorithm
+
 """
+"""
+
+START_TIMESTAMP = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+def standardHillclimber(network: RailNetwork, maxRoutes: int, maxDuration: float, 
+    targetFolder: str ="results", runName: str = "solution", convergenceLimit: int = 5000, 
+    recordAll: bool = False):
+    
+    convergence = 0
+    iteration = 1
+    highest = 0
+
+    bestNetwork = network
+
+    scores: List[Dict[str, Union[int, float]]] = []
+
+    while convergence <= convergenceLimit:
+        print(f"iteration: {iteration}")
+
+        workNetwork = deepcopy(bestNetwork)
+
+        
+def climbStep
