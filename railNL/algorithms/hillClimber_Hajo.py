289f4915a087315501558de5c154a965eb9648ae
Hajo <hajo-groen@mail.com>
2023-01-24 22:24:33 +0100

    formatting of function arguments code formatting implemented annealingFunction as a callable in annealingClimber
    Added cooling schemes for hillclimber, logarithmic, geometric,
    and linear
    
    Source for cooling schemes:
    Mahdi, W.; Medjahed, S. A.; Ouali, M. Performance Analysis of Simulated Annealing Cooling
        Schedules in the Context of Dense Image Matching. Computación y Sistemas, 2017, 21.
        https://doi.org/10.13053/cys-21-3-2553.

--- railNL/algorithms/hillClimber_Hajo.py
+++ railNL/algorithms/hillClimber_Hajo.py
@@ -3,6 +3,7 @@ import datetime
 import time
 
 from copy import deepcopy
+from math import exp, log10
 
 from classes.railNetwork import RailNetwork
 from classes.route import Route
@@ -18,12 +19,62 @@ import algorithms.random_hajo as randomAlgorithm
 
 START_TIMESTAMP = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-def routeHillclimber(network: RailNetwork, maxRoutes: int, maxDuration: float, 
-    targetFolder: str ="results", runName: str = "soluionHill", convergenceLimit: int = 5000,
-    randomIterations: int = 5000, recordAll: bool = False) -> RailNetwork:
+def routeHillclimber(
+        network: RailNetwork, 
+        maxRoutes: int, 
+        maxDuration: float, 
+
+        targetFolder: str ="results", 
+        runName: str = "solutionHill", 
+
+        convergenceLimit: int = 5000,
+        randomIterations: int = 5000, 
+
+        recordAll: bool = False,
+        **_
+    ) -> RailNetwork:
+    """Runs the annealing climber as a standard hillclimber"""
+
+    annealingClimber(
+        network = network, 
+        maxRoutes = maxRoutes, 
+        maxDuration = maxDuration,
+
+        stepfunction = routeClimb, 
+        annealingFunction=hillClimbCoolingScheme, 
+
+        targetFolder = targetFolder, 
+        runName = runName, 
+
+        convergenceLimit = convergenceLimit, 
+        randomIterations = randomIterations, 
+        recordAll = recordAll
+        )
+
+
+def annealingClimber(
+        network: RailNetwork, 
+        maxRoutes: int, 
+        maxDuration: float,
+
+        stepFunction: callable,
+
+        annealingFunction: callable,
+
+        initialTemperature: float = 0,
+        coolingConstant: float = 0,
+
+        targetFolder: str ="results", 
+        runName: str = "soluionHill", 
+
+        convergenceLimit: int = 5000,
+        randomIterations: int = 5000, 
+
+        recordAll: bool = False
+    ) -> RailNetwork:
     """
     A hillclimber that takes any network and attempts to optimize it by adding, removing or 
-    replacing random routes.
+    replacing random routes. Base for a simulated annealing algorithm
 
     Args:
         network (RailNetwork):
@@ -37,11 +88,11 @@ def routeHillclimber(network: RailNetwork, maxRoutes: int, maxDuration: float,
     
     Returns: The optimized network
     """
-
     convergence = 0
-    iteration = 1
+    iteration = 0
 
     bestNetwork = network
+    scores: List[Dict[str, Union[int, float]]] = []
 
     if randomIterations:
         print("generating random solution")
@@ -50,26 +101,24 @@ def routeHillclimber(network: RailNetwork, maxRoutes: int, maxDuration: float,
                                                      maxDuration,
                                                      randomIterations
                                                     )
-        highestScore = bestNetwork.score()
-
-    scores: List[Dict[str, Union[int, float]]] = []
 
     highestScore = bestNetwork.score()
-    
     scores.append({"iteration":iteration, "score":highestScore})
     
     while convergence <= convergenceLimit:
         print(f"iteration: {iteration}")
 
         workNetwork = deepcopy(bestNetwork)
-        climbStep(workNetwork, maxRoutes, maxDuration)
+        stepFunction(workNetwork, maxRoutes, maxDuration)
 
         newScore = workNetwork.score()
         
-        # When a new highscore is found, print to terminal, export the solution and append the score
-        # to the scorelist.
-        # Convergence set back to zero.
-        if newScore > highestScore:
+        # score >= highest or annealingFunction returns True
+        if newScore >= highestScore or annealingFunction((highestScore - newScore), 
+                                                          initialTemperature,
+                                                          iteration,
+                                                          coolingConstant,
+                                                          ):
             print(f"new best found: {newScore}")
 
             highestScore = newScore
@@ -82,15 +131,15 @@ def routeHillclimber(network: RailNetwork, maxRoutes: int, maxDuration: float,
         elif recordAll:
             scores.append({"iteration":iteration, "score":newScore})
 
-        iteration += 1
         convergence += 1
+        iteration += 1
 
-        randomAlgorithm.exportScores(scores, targetFolder, runName, START_TIMESTAMP)
+    randomAlgorithm.exportScores(scores, targetFolder, runName, START_TIMESTAMP)
 
     return bestNetwork
 
-
-def climbStep(network: RailNetwork, maxRoutes: int, maxDuration: float) -> None:
+# Stepfunction
+def routeClimb(network: RailNetwork, maxRoutes: int, maxDuration: float) -> None:
     """
     Either adds, removes or replaces a random route.
 
@@ -121,3 +170,65 @@ def climbStep(network: RailNetwork, maxRoutes: int, maxDuration: float) -> None:
 def removeRoute(network: RailNetwork):
     randomRoute = random.choice(network.listRoutes())
     network.delRoute(randomRoute.getID())
+
+
+def hillClimbCoolingScheme(*_):
+    """Always returns false, as a true hillclimber does not accept worse results"""
+    return False
+
+
+def annealingProbability(scoreDiff: float, temperature: float):
+    probability = exp(-(scoreDiff / temperature))
+    if random.random() <= probability:
+        return True
+    
+    return False
+
+
+def logarithmic(scoreDiff:float, _, iteration: int, constant: float):
+    """
+    Decreases temperature logarithmically over iterations.
+
+    Source:
+    Mahdi, W.; Medjahed, S. A.; Ouali, M. Performance Analysis of Simulated Annealing Cooling 
+    Schedules in the Context of Dense Image Matching. Computación y Sistemas, 2017, 21. 
+    https://doi.org/10.13053/cys-21-3-2553.
+    """
+    currentTemperature = constant / log10(1 + iteration)
+
+    return annealingProbability(scoreDiff, currentTemperature)
+
+
+def geometricCooling(scoreDiff:float, initialTemp: float, iteration: int, powerBase: float) -> float:
+    """
+    Decreases temperature geometrically over iterations.
+    
+    Args:
+        scoreDiff (float): Difference in score between the old state and the new state.
+        initialTemp (float): the initial temperature of the annealing function
+        
+
+    Source:
+    Mahdi, W.; Medjahed, S. A.; Ouali, M. Performance Analysis of Simulated Annealing Cooling 
+    Schedules in the Context of Dense Image Matching. Computación y Sistemas, 2017, 21. 
+    https://doi.org/10.13053/cys-21-3-2553.
+    """
+    currentTemperature = initialTemp * (powerBase ** iteration)
+
+    return annealingProbability(scoreDiff, currentTemperature)
+
+def linearCooling(scoreDiff:float, initialTemp: float, iteration: int, coolingSpeed: float):
+    """
+    Decreases temperature linearily over iterations.
+
+    Source:
+    Mahdi, W.; Medjahed, S. A.; Ouali, M. Performance Analysis of Simulated Annealing Cooling 
+    Schedules in the Context of Dense Image Matching. Computación y Sistemas, 2017, 21. 
+    https://doi.org/10.13053/cys-21-3-2553.
+    """
+    currentTemperature = initialTemp - coolingSpeed * iteration
+
+    if currentTemperature <= 0:
+        return False
+    
+    return annealingProbability(scoreDiff, currentTemperature)
